80dc4e1b5dd5558fb90aa10aa2ee6b613654d228
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-02 18:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function fetchBooks() {
     return fetch('https://anapioficeandfire.com/api/books').then(function(response) {
-      response.json().then(function(json) {
+        response.json();
+    })
+      .then(function(json) {
         renderBooks(json);
       });
-    })
 }
 
 function renderBooks(books) {
